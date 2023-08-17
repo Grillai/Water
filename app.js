c36b9dbc210707f73b2b7d4f46b643e59a619568
Grillai <53846286+Grillai@users.noreply.github.com>
2023-08-18 00:47:29 +0200

    Water

--- app.js
+++ app.js
@@ -4,3 +4,44 @@ const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+
+
+const images = document.querySelectorAll('.carousel-image');
+  const secondImages = document.querySelectorAll('.second-carousel-image');
+  const secondCarouselText = document.querySelector('.second-carousel-text');
+  const dots = document.querySelectorAll('.dot');
+  let currentImageIndex = 0;
+
+  const texts = [
+    "Lasciati ispirare dall'oceano,  resta idratato con stile.",
+    "Sali in cima, con la nostra borraccia smart.",
+    "Caldo estremo, idratazione intelligente."
+  ];
+
+  function showImage(index) {
+    images.forEach(image => image.style.display = 'none');
+    secondImages.forEach(image => image.style.display = 'none');
+    
+    images[index].style.display = 'block';
+    secondImages[index].style.display = 'block';
+    currentImageIndex = index;
+    
+    secondCarouselText.textContent = texts[index];  // Aggiunge il testo al secondo carosello
+
+    dots.forEach((dot, dotIndex) => {
+      if (dotIndex === index) {
+        dot.style.backgroundColor = 'black';
+      } else {
+        dot.style.backgroundColor = 'LightGray';
+      }
+    });
+  }
+
+  dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+      showImage(index);
+    });
+  });
+
+  showImage(currentImageIndex);
